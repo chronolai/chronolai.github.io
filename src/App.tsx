be661a4decf062ef5876e262aba45fcccb83c68e
Chrono Lai <x941i6c04@gmail.com>
2026-06-16 00:11:22 +0800

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="hello">
+      <h1>Hello, I'm Chrono 👋</h1>
+      <p>Welcome to my personal site. More coming soon.</p>
+    </main>
+  )
+}
+
+export default App
